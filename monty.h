fd06bfbf4bcfea68c55eeb89b0f070d8d238bc32
jayricka <jayricka17@gmail.com>
2023-06-23 01:20:45 +0300

    free memory

--- monty.h
+++ monty.h
@@ -35,5 +35,8 @@ typedef struct instruction_s
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 
+/* Other function declarations */
+void free_stack(stack_t *stack);
+
 #endif /* MONTY_H */
 
--- opcode_push.c
+++ opcode_push.c
@@ -4,27 +4,22 @@
 
 void push(stack_t **stack, unsigned int line_number)
 {
-    stack_t *new_node;
+        stack_t *new_node;
 
-    (void)line_number;
+        new_node = malloc(sizeof(stack_t));
+        if (new_node == NULL)
+        {
+                fprintf(stderr, "Error: malloc failed\n");
+                free_stack(*stack);
+                exit(EXIT_FAILURE);
+        }
 
-    new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    new_node->n = line_number;
-    new_node->prev = NULL;
-    if (*stack == NULL)
-    {
-        new_node->next = NULL;
-    }
-    else
-    {
+        new_node->n = line_number;
+        new_node->prev = NULL;
         new_node->next = *stack;
-        (*stack)->prev = new_node;
-    }
-    *stack = new_node;
+
+        if (*stack != NULL)
+                (*stack)->prev = new_node;
+
+        *stack = new_node;
 }
