3544b8dfd386e9edcb7e1dce931b0205ce1d23d1
jayricka <jayricka17@gmail.com>
2023-06-23 00:40:03 +0300

    debugs

--- monty.h
+++ monty.h
@@ -1,18 +1,14 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-
 /**
- * struct stack_s - Doubly linked list representation of a stack (or queue)
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the stack (or queue)
- * @next: Pointer to the next element of the stack (or queue)
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
  *
- * Description: Doubly linked list node structure
- * for stack and queue implementation
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
@@ -20,6 +16,7 @@ typedef struct stack_s
     struct stack_s *prev;
     struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -30,17 +27,13 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-/* Function declarations */
-void execute_instruction(char *instruction, stack_t **stack,
-                         unsigned int line_number);
-void execute_file(const char *filename);
-void push(stack_t **stack, int n);
-void pall(stack_t **stack);
-void free_stack(stack_t *stack);
-void pint(stack_t **stack)
+
+/* Function prototypes */
+void push(stack_t **stack, int value);
+void pall(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
 
--- opcode_pint.c
+++ opcode_pint.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "monty.h"
-#include "opcode_pint.h"
 
 /**
  * pint - Print the value at the top of the stack
  * @stack: Pointer to the top of the stack
  */
-void pint(stack_t **stack)
+void pint(stack_t **stack, unsigned int line_number)
 {
     if (*stack == NULL)
     {
