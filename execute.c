8632a0eb782cfe87b66afd0f094194c973068d1b
jayricka <jayricka17@gmail.com>
2023-06-23 03:32:32 +0300

    debugs

--- execute.c
+++ execute.c
@@ -3,25 +3,26 @@
 #include <string.h>
 #include "monty.h"
 
-void process_instruction(char *opcode, char *data, unsigned int line_number, stack_t **stack)
+void process_instruction(char *opcode, char *data, unsigned int line_number,
+stack_t **stack)
 {
-	if (strcmp(opcode, "push") == 0)
-	{
-		/* Example implementation for push opcode */
-		/* Convert data to an integer and push it onto the stack */
-		int value = atoi(data);
-		push(stack, value);
-	}
-	else if (strcmp(opcode, "pall") == 0)
-	{
-		/* Example implementation for pall opcode */
-		/* Print all the elements in the stack */
-		pall(stack);
-	}
-	else
-	{
-		/* Handle unknown opcode */
-		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-		exit(EXIT_FAILURE);
-	}
+if (strcmp(opcode, "push") == 0)
+{
+/* Example implementation for push opcode */
+/* Convert data to an integer and push it onto the stack */
+int value = atoi(data);
+push(stack, value);
+}
+else if (strcmp(opcode, "pall") == 0)
+{
+/* Example implementation for pall opcode */
+/* Print all the elements in the stack */
+pall(stack);
+}
+else
+{
+/* Handle unknown opcode */
+fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+exit(EXIT_FAILURE);
+}
 }
--- push.c
+++ push.c
@@ -2,29 +2,34 @@
 #include <stdlib.h>
 #include "monty.h"
 
+/**
+* push - Pushes a new node onto the stack
+* @stack: Double pointer to the stack
+* @value: Value to be stored in the new node
+*/
 void push(stack_t **stack, int value)
 {
-    stack_t *new_node;
+stack_t *new_node;
 
-    new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+new_node = malloc(sizeof(stack_t));
+if (new_node == NULL)
+{
+fprintf(stderr, "Error: malloc failed\n");
+exit(EXIT_FAILURE);
+}
 
-    new_node->n = value;
-    new_node->prev = NULL;
+new_node->n = value;
+new_node->prev = NULL;
 
-    if (*stack != NULL)
-    {
-        new_node->next = *stack;
-        (*stack)->prev = new_node;
-    }
-    else
-    {
-        new_node->next = NULL;
-    }
+if (*stack != NULL)
+{
+new_node->next = *stack;
+(*stack)->prev = new_node;
+}
+else
+{
+new_node->next = NULL;
+}
 
-    *stack = new_node;
+*stack = new_node;
 }
